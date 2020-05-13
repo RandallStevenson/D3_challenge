381e931af3778cecd72d5b6655fb1eeb5d3f4818
RandallStevenson <59617625+RandallStevenson@users.noreply.github.com>
2020-05-13 17:42:09 -0500

    Add files via upload

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,3 +1,5 @@
+// I chose to compare obesity to smoking and also look at them regionally
+
 // I added a column of colors by region to the data file - manually
 // The objective is to graph of obesity % vs smoking %
 // Color code is 
@@ -8,75 +10,96 @@
 //      orange = southwest 
 //      purple = west
 
+// Much of this was modified from Youtube tutorials on d3 by Curran Kelleher
 
-// Read the data from the csv file and print to console
+// Initial assignments of values
+
+var svg=d3
+   .select("body")
+   .append("svg")
+   .attr("height", 700)
+   .attr("width", 700);
+
+var width = +svg.attr("width");
+var height = +svg.attr("height");
+
+var show = data => {
+    var margin={ top:50, bottom:50, left:50, right:50}
+   // rather than padding, I added about 10% to the scales 
+   
+   // smokers range 9%-27%
+    var xScale = d3.scaleLinear()
+        .domain([5,30])
+        .range([0,width-margin.left-margin.right]);
+    var xAxis=d3.axisLeft(xScale);
+
+    var yScale = d3.scaleLinear()
+    // obesity range is 21%-36%    
+        .domain([20,40])
+        .range([0,height-margin.top-margin.bottom]);
+    
+    var yAxis=d3.axisLeft(yScale);
+
+    var g=svg.append('g')
+    //    .attr("transform","translate(50,50)");
+    //g.append('g').call(d3.axisLeft(yScale)-15);
+    //g.append('g').call(d3.axisBottom(xScale)-15);
+
+    svg.selectAll("circle")
+    // statedata(statedata)
+        .enter()
+        .append("circle")
+        .attr("cx",d => xScale(d.smokes))
+        .attr("cy",d => yScale(d.obesity))
+        .attr("fill",d => d.region)
+        .attr("r",30)
+};
+
+// axis labeling found in stack overflow
+
+svg.append("text")
+    .attr("class","x label")
+    .attr("text-anchor", "end")
+    .attr("x",width)
+    .attr("y", height - 6)
+    .text("Smokers % of Population");
+
+svg.append("text")
+    .attr("class","y label")
+    .attr("text-anchor", "end")
+    .attr("y",6)
+    .attr("dy", "25px")
+    .text("Obesity as a % of Population");
+
+
 // I stored the file in the github.io server to be able to read, since I was
 // having trouble with using a local server for this and to test the page.
 
-d3.csv("https://RandallStevenson.github.io/data.csv").then(function(statedata) {
+
+// Read the data from the csv file and print to console
+// and convert obesity and smoker to numbers, then log statedata
+d3.csv("https://RandallStevenson.github.io/data.csv").then(statedata => {
+    statedata.forEach(state => {
+        state.obesity = +state.obesity;
+        state.smokes = +state.smokes;
+    })
         console.log(statedata);
 
-// log a list of state abbreviations
-var abbr=statedata.map(data => data.abbr);
-console.log("State abbreviations: ",abbr);
-
-// log a list of region colors, smokers and obesity
-var regions=statedata.map(data => data.region);
-console.log("Regional colors: ",regions);
-
-// log a list of % smokers as numbers
-var smoker=statedata.map(data => +data.smokes);
-console.log("Smokers: ",smoker);
-
-// log a list of % obesity as numbers
-var obese=statedata.map(data => +data.obesity);
-console.log("Obesity: ",obese);
+        show(statedata);
 });
 
-var svgWidth=700;
-var svgHeight=700;
-
-// set multiplier to 10 for cx and cy to fit on graph, 
-// because all percentages are between 9 and 50
-
-var scale=10;
-
-// obseity rates range from 21-36%
-var yScale=d3.scaleLinear()
-    .domain([20,40])
-    .range([100,700])
-// will need to multiply the obsiety rates by 300 and add 100 to scale ys
-
-// smoking rates range from 9-27%    
-var xScale=d3.scaleLinear()
-    .domain([0,30])
-    .range([100,700])
-// will need to multiply the smoking rates by 200 and add 100 to scale xs
 
 
-function printpoint(x,y,c,a) {
 
-};
-
-
-var yAxis=d3.axisLeft(yScale);
-var xAxis=d3.axisBottom(xScale);
-    
-//var svg=d3
-//    .select("body")
-//    .append("svg")
-//    .attr("height", svgHeight)
-//    .attr("width", svgWidth);
 
 // this was modified from information on stackoverflow
 
-var cirle=elemEnter.append("circle")
-    .attr("r",10)
-    // color the circles based on the region they are in
-    .attr("fill",function(d){return d.region})
-    .attr("cx",function(d){return 50+10*(+d.smokes)})
-    .attr("cy",function(d){return 50+10*(+d.obesity)});
+//var cirle=elemEnter.append("circle")
+//    .attr("r",10)
+//    .attr("fill",function(d){return d.region})
+//    .attr("cx",function(d){return 50+10*(+d.smokes)})
+//    .attr("cy",function(d){return 50+10*(+d.obesity)});
 
-elemEnter.append("text")
-.attr("dx", function(d){return -5})
-.text(function(d){return d.abbr})
+//elemEnter.append("text")
+//.attr("dx", function(d){return -5})
+//.text(function(d){return d.abbr})
